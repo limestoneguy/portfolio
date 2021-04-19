0b26265c05501a090b0dcfd3759f0e7feeb86699
limestoneguy <kumar.rishabh007.rk@gmail.com>
2021-04-20 01:36:12 +0530

    section 1 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './screens/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: PortfolioComponent, pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './screens/portfolio/portfolio.component';
+import { FaceComponent } from './screens/portfolio/components/face/face.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    FaceComponent
   ],
   imports: [
     BrowserModule,
